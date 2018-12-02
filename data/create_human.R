e96ba4c3d31f09d15dbbb5eb7bd32909bdb35d34
ehstahl <43642766+ehstahl@users.noreply.github.com>
2018-12-02 20:07:23 +0200

    Week 5

--- data/create_human.R
+++ data/create_human.R
@@ -1,44 +1,51 @@
+Author: "Erik Stahl"
+Date: "30.11.2018"
+#Script for the data wrangling exercise of week 5
+#Data source: http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt
 
-hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+library(dplyr)
+library(stringr)
 
-gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+human <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt",
+                    sep=",",
+                    header=TRUE)
 
-dim(hd)
-dim(gii)
+# dim(human)
+# str(human)
 
-str(hd)
-str(gii)
+#The table has 195 observations (countries in this case) and 19 variables.
+#The Human development index (HDI) is meant to give a more detailed view of the growth of countries.
+#By including factors of human capabilities and quality of life, it can raise questions about how equally wealthy countries (GNI at same level) can differ in terms of quality of life.
+#In the data the quality of life factors are made visible in variables such as life expectancy (Life.Exp), maternal mortality rate (Mat.Mor) and share of seats in parliament held by women (Parli.F).
+#The last one is maybe not an explicit quality of life factor, but indeed an inequality one. HDI also addresses inequality.
 
-summary(hd)
-summary(gii)
+#Next: mutate variable GNI into a numeric.
 
-hd$HDI <- hd$Human.Development.Index..HDI.
-hd$Human.Development.Index..HDI. <- NULL
+human$GNI <- str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric()
 
-hd$GNIperCap <- hd$Gross.National.Income..GNI..per.Capita
-hd$Gross.National.Income..GNI..per.Capita <- NULL
+#Exclude unwanted variables.
 
-hd$GNIminusHDIrank <- hd$GNI.per.Capita.Rank.Minus.HDI.Rank
-hd$GNI.per.Capita.Rank.Minus.HDI.Rank <- NULL
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- dplyr::select(human, one_of(keep))
 
-gii$GII <- gii$Gender.Inequality.Index..GII.
-gii$Gender.Inequality.Index..GII. <- NULL
+#Get rid of rows with one or more missing values (NA).
 
-gii$PercentinParliament <- gii$Percent.Representation.in.Parliament
-gii$Percent.Representation.in.Parliament <- NULL
+human_minusNA <- filter(human, complete.cases(human) == TRUE)
 
-gii$SecEdFem <- gii$Population.with.Secondary.Education..Female.
-gii$Population.with.Secondary.Education..Female. <- NULL
+#View the last 10 observations in the new set.
 
-gii$SecEdMale <- gii$Population.with.Secondary.Education..Male.
-gii$Population.with.Secondary.Education..Male. <- NULL
+tail(human_minusNA, n=10)
 
-gii$LabParFem <- gii$Labour.Force.Participation.Rate..Female.
-gii$Labour.Force.Participation.Rate..Female. <- NULL
+#Remove the last 7 observations, because they are regions and not countries.
 
-gii$LabParMale <- gii$Labour.Force.Participation.Rate..Male.
-gii$Labour.Force.Participation.Rate..Male. <- NULL
+reg <- nrow(human_minusNA) - 7
+human_minusNA <- human_minusNA[1:reg,]
 
-str(hd)
+#Set the names from the countries variable as rownames.
 
-str(gii)
+rownames(human_minusNA) <- human_minusNA$Country
+human_minusNA$Country <- NULL
+
+dim(human_minusNA)
+
+write.csv(human_minusNA, file="create_human.csv", row.names = TRUE)
